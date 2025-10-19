44dbbf851beda47f2974217a88af699e01f6b4cc
Alemaann <alemanayala2024@gmail.com>
2025-10-19 15:54:33 -0600

    Create Alemaann-00.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Alemaann-00.py
@@ -0,0 +1,21 @@
+#Sitio oficial de Python: https://www.python.org/
+#Este es un comentario de linea (solo para una linea)
+
+"""
+Este es un comentario de bloque o de 
+lías multiples
+
+"""
+x = 30 #Esto es una variable declarada
+
+#variables representando todos los tipos de datos primitivos
+
+nombre = str("Manuel") #Esto es una variable de tipo string
+edad = int(19) #Esto es una variable de tipo entero
+estatura = float(1.60) #Esto es una variable de tipo folat
+jugadorFutbol = True #Esto es una variable de tipo booleano
+
+#Imprimir mensajes en pantalla
+lenguaje = "Python"
+
+print("Hola" + " " + lenguaje)
